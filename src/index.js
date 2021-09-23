deb55617c1b7fe7ee8704e7d1a5d8cde044f331b
Josh Justice <me@codingitwrong.com>
2021-09-23 09:46:20 -0400

    Add package version of files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const useStyleQueries = require('./useStyleQueries.js');
+const screenWidthMin = require('./queries/screenWidthMin.js');
+
+module.exports = {
+  useStyleQueries,
+  screenWidthMin,
+};
