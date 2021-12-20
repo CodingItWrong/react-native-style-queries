6fff9613ce4e0d4c98356e9be752d076a98cf242
Josh Justice <me@codingitwrong.com>
2021-12-20 17:11:36 -0500

    No styles case

--- src/useStyleQueries.js
+++ src/useStyleQueries.js
@@ -1,28 +1,7 @@
-const {useWindowDimensions} = require('react-native');
+// const {useWindowDimensions} = require('react-native');
 
-function useStyleQueries(styleConfig) {
-  const {width} = useWindowDimensions();
-  const predicateArgument = {screenWidth: width};
-
-  const styleEntries = Object.entries(styleConfig);
-
-  const styleEntriesToApply = styleEntries.map(([key, styleClauses]) => {
-    // TODO: determine if any entry can be a "defaults" object
-    const styleObject = styleClauses.reduce((acc, clause) => {
-      const [predicate, conditionalStyleObject] = clause;
-
-      const doesClauseMatch = predicate(predicateArgument);
-      if (doesClauseMatch) {
-        return {...acc, ...conditionalStyleObject};
-      } else {
-        return acc;
-      }
-    });
-
-    return [key, styleObject];
-  });
-
-  return Object.fromEntries(styleEntriesToApply);
+function useStyleQueries(/*styleConfig*/) {
+  return {};
 }
 
 module.exports = useStyleQueries;
--- src/useStyleQueries.test.js
+++ src/useStyleQueries.test.js
@@ -1,47 +1,15 @@
 const useStyleQueries = require('./useStyleQueries');
-const {useWindowDimensions} = require('react-native');
+// const {useWindowDimensions} = require('react-native');
 
-jest.mock('react-native', () => ({
-  useWindowDimensions: jest.fn(),
-}));
+// jest.mock('react-native', () => ({
+//   useWindowDimensions: jest.fn(),
+// }));
 
 describe('useStyleQueries', () => {
-  const styleConfig = {
-    text: [
-      {
-        fontFamily: 'Arial',
-        fontSize: 12,
-      },
-      [
-        ({screenWidth}) => screenWidth >= 375,
-        {
-          fontSize: 18,
-          color: 'red',
-        },
-      ],
-    ],
-  };
-
-  it('uses the base styles when no conditions match', () => {
-    useWindowDimensions.mockReturnValue({width: 320});
-    const result = useStyleQueries(styleConfig);
-    expect(result).toEqual({
-      text: {
-        fontFamily: 'Arial',
-        fontSize: 12,
-      },
-    });
-  });
-
-  it('merges conditional styles on top of the base styles', () => {
-    useWindowDimensions.mockReturnValue({width: 375});
-    const result = useStyleQueries(styleConfig);
-    expect(result).toEqual({
-      text: {
-        fontFamily: 'Arial',
-        fontSize: 18,
-        color: 'red',
-      },
+  describe('when no styles are passed', () => {
+    it('returns an empty style object', () => {
+      const styles = useStyleQueries({});
+      expect(styles).toEqual({});
     });
   });
 });
